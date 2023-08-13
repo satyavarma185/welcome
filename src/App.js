dff80d67e1e8b3af2eb651d8bd8af7cdfaeabdab
satyavarma185 <satyavarma135@gmail.com>
2023-08-13 08:16:16 +0530

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      Welcome to my first react page
+      <h1>Welcome to my first react page</h1>
     </div>
   );
 }
